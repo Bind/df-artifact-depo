9e52f2411907054d03e5511d2190118834850602
bind <douglasjbinder@gmail.com>
2022-02-20 13:45:37 -0600

    chore: formatting

--- src/Depo.sol
+++ src/Depo.sol
@@ -3,52 +3,73 @@ pragma solidity >=0.8.0;
 import {ERC721TokenReceiver, ERC721} from "solmate/tokens/ERC721.sol";
 import "./DFMock.sol";
 
-// Todo: Handle depositing same item multiple times
-
 contract Depo is ERC721TokenReceiver {
-    address immutable DF_CORE;
-    mapping(address => bool) public masters_at_arms; // Can withdraw
-    mapping(uint256 => bool) public deposits; // Artifacts currently deposited
-    mapping(address => uint256[]) public records; // who deposited what
+    /*///////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
     modifier onlyMasterAtArms() {
-        require(masters_at_arms[msg.sender], "ONLY_MAA");
+        require(mastersAtArms[msg.sender], "ONLY_MAA");
         _;
     }
 
-    constructor(address dfCore, address[] memory blessed) {
-        DF_CORE = dfCore;
+    /*///////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event Deposit(address indexed depositor, uint256 indexed tokenId);
+
+    event Withdrawl(address indexed withdrawer, uint256 indexed tokenId);
+
+    /*///////////////////////////////////////////////////////////////
+                                STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    address public immutable dfCore;
+
+    mapping(address => bool) public mastersAtArms; // Can withdraw
+
+    mapping(uint256 => bool) public deposits; // Artifacts currently deposited
+
+    /*///////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address core, address[] memory blessed) {
+        dfCore = core;
         for (uint256 i = 0; i < blessed.length; i++) {
-            masters_at_arms[blessed[i]] = true;
+            mastersAtArms[blessed[i]] = true;
         }
     }
 
-    function receipts(address depositor)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        return records[depositor];
-    }
+    /*///////////////////////////////////////////////////////////////
+                             ACCESS CONTROL
+    //////////////////////////////////////////////////////////////*/
 
     function promote(address pleb) public onlyMasterAtArms {
-        masters_at_arms[pleb] = true;
+        mastersAtArms[pleb] = true;
     }
+
+    /*///////////////////////////////////////////////////////////////
+                               DEPO LOGIC
+    //////////////////////////////////////////////////////////////*/
 
     function withdrawArtifact(uint256 tokenId) public onlyMasterAtArms {
         require(deposits[tokenId], "TOKEN NOT DEPOSTED");
-        DFMock(DF_CORE).safeTransferFrom(address(this), msg.sender, tokenId);
+        DFMock(dfCore).safeTransferFrom(address(this), msg.sender, tokenId);
         deposits[tokenId] = false;
+        emit Withdrawl(msg.sender, tokenId);
     }
 
     function onERC721Received(
-        address operator,
+        address,
         address from,
         uint256 tokenId,
-        bytes calldata data
+        bytes calldata
     ) external returns (bytes4) {
-        require(msg.sender == DF_CORE, "NOTCORE");
+        require(msg.sender == dfCore, "NOTCORE");
         deposits[tokenId] = true;
-        records[from].push(tokenId);
+        emit Deposit(from, tokenId);
         return this.onERC721Received.selector;
     }
 }
